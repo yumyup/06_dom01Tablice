6d0a027a09121b833df499d5b10ca64252972403
yupi <grafik@10g.pl>
2017-03-21 22:20:51 +0100

    poprawienie kodu sum1++, sum2++ itd

--- src/Tablica.java
+++ src/Tablica.java
@@ -2,9 +2,12 @@ import java.util.Random;
 
 public class Tablica {
     public static void main(String[] args) {
-        int sum = 0;
+        int sum1 = 0;
+        int sum2 = 0;
+        int sum3 = 0;
+        int sum4 = 0;
 
-        int[] tablica = new int[5];
+        int[] tablica = new int[100];
         Random random = new Random();
 
         for (int i = 0; i < tablica.length; i++) {
@@ -14,27 +17,23 @@ public class Tablica {
         for (int i : tablica) {
 
             if (i < 50) {
-                sum++;
-
+                sum1++;
             }
 
             if (i > 50) {
-                 sum++;
-
+                sum2++;
             }
             if (i % 2 == 0) {
-                sum = sum++;
-
+                 sum3++;
             }
             if (i % 2 != 0) {
-                sum = sum++;
-
+                sum4++;
             }
         }
-        System.out.println("i<50 " + sum);
-        System.out.println("i>50 " + sum);
-        System.out.println("parzyste " + sum);
-        System.out.println("nieparzyste " + sum);
+        System.out.println("i<50 " + sum1);
+        System.out.println("i>50 " + sum2);
+        System.out.println("parzyste " + sum3);
+        System.out.println("nieparzyste " + sum4);
     }
 }
 
